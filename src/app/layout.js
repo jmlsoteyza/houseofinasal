68034f988529dbf20750c5cb6927d27130220ce9
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-02 17:11:15 +0400

    add more font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,13 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Montserrat } from 'next/font/google';
 import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
+  subsets: ['latin']
+});
+
+const montserrat = Montserrat({
+  variable: '--font-montserrat',
   subsets: ['latin']
 });
 
@@ -20,7 +25,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${montserrat.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
