c5479b55e1820e401ea8fb945cdda65d8f5f8560
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-05 16:01:24 +0400

    minor changes

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 w-full bg-white shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] z-50">
-      <div className="max-w-[1450px] mx-auto flex items-center px-6 py-6">
+      <div className="max-w-[1450px] mx-auto flex items-center px-6 py-4">
         <div className="flex items-center flex-1">
           <div className="w-full max-w-[10.1875rem] cursor-pointer">
             <Image
@@ -15,7 +15,7 @@ export default function Navbar() {
             />
           </div>
 
-          <ul className="flex gap-3 ml-8 font-bold">
+          <ul className="flex gap-3 ml-14 font-bold">
             <li>Menu</li>
             <li>Location</li>
             <li>Home</li>
@@ -30,5 +30,3 @@ export default function Navbar() {
     </div>
   );
 }
-
-
