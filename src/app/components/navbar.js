2362029a3bcdb2b0930ca955c0fd9b72c1b5b7ba
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-05 05:18:58 +0400

    update logo

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -3,7 +3,15 @@ import Image from 'next/image';
 export default function Navbar() {
   return (
     <div className="">
-      <p>Test git</p>
+      <div className="w-full max-w-[10.1875rem] h-full">
+        <Image
+          src="/images/test.svg"
+          alt="hof-logo"
+          width={159}
+          height={59}
+          className="w-full rounded-sm"
+        />
+      </div>
     </div>
   );
 }
