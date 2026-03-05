3e81257d78d17c404f2902a9cbd23ec5c0cc9237
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-05 16:06:30 +0400

    add link into logo

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,15 +6,17 @@ export default function Navbar() {
     <div className="fixed top-0 left-0 w-full bg-white shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] z-50">
       <div className="max-w-[1450px] mx-auto flex items-center px-6 py-4">
         <div className="flex items-center flex-1">
-          <div className="w-full max-w-[10.1875rem] cursor-pointer">
-            <Image
-              src="/images/logo.svg"
-              alt="hof-logo"
-              width={163}
-              height={59}
-              className="w-full rounded-sm"
-            />
-          </div>
+          <Link href="/">
+            <div className="w-full max-w-[10.1875rem] cursor-pointer">
+              <Image
+                src="/images/logo.svg"
+                alt="hof-logo"
+                width={163}
+                height={59}
+                className="w-full rounded-sm"
+              />
+            </div>
+          </Link>
 
           <ul className="flex gap-6 ml-14 font-bold">
             <Link href="/menu">
