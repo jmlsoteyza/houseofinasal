4b9d2b57939bb0f19d4dedc081b190bd516d8c0d
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-05 22:32:54 +0400

    call us styles

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 w-full bg-white shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] z-50">
-      <div className="max-w-[1450px] mx-auto flex items-center px-6 py-4">
+      <div className="max-w-[1450px] mx-auto flex items-center px-6 py-2 ">
         <div className="flex items-center flex-1">
           <Link href="/">
             <div className="w-full max-w-[10.1875rem] cursor-pointer">
@@ -31,9 +31,9 @@ export default function Navbar() {
           </ul>
         </div>
 
-        <div className="flex flex-col font-bold">
-          <p>Call Us</p>
-          <p>0582416753</p>
+        <div className="flex flex-col font-bold mr-8 text-lg">
+          <p className="font-primary tracking-widest text-right">Call Us</p>
+          <p className="text-primary text-xl tracking-widest font-heading">0582416753</p>
         </div>
       </div>
     </div>
