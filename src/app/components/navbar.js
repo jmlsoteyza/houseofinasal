2808df836d62170f59dc64d09c941e03d089a49f
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-06 13:11:49 +0400

    minor update in navbar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,19 +6,17 @@ export default function Navbar() {
     <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
       {/* shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] save shadow */}
       <div className="max-w-[1250px] mx-auto flex items-center px-6 py-3 justify-between">
-        <Link href="/">
-          <div className="w-full max-w-[10.1875rem] cursor-pointer">
-            <Image
-              src="/images/logo.svg"
-              alt="hof-logo"
-              width={163}
-              height={59}
-              className="w-full rounded-sm"
-            />
-          </div>
+        <Link href="/" className="cursor-pointer">
+          <Image
+            src="/images/logo.svg"
+            alt="hof-logo"
+            width={163}
+            height={59}
+            className="rounded-sm"
+          />
         </Link>
 
-        <ul className="flex gap-12 mr-4 font-medium font-ui items-center text-[0.938rem]">
+        <ul className="flex gap-12 mr-4 font-medium font-ui items-center text-[0.969rem]">
           <li>
             <Link href="/menu">Menu</Link>
           </li>
@@ -30,9 +28,13 @@ export default function Navbar() {
           </li>
         </ul>
 
-        <div className="flex flex-col font-bold">
-          <p className="font-primary tracking-widest text-right font-ui">Call Us</p>
-          <p className="text-primary text-lg tracking-[0.2rem] font-heading">0582416753</p>
+        <div className="flex flex-col">
+          <p className="tracking-widest text-sm font-semibold text-right font-ui leading-[0.78]">
+            Call Us
+          </p>
+          <p className="text-primary text-lg tracking-[0.2rem] font-normal font-heading">
+            0545617393
+          </p>
         </div>
       </div>
     </div>
