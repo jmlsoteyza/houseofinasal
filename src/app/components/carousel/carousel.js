c2f6e0f2fd250bed5614d4acd0b7678e5b61306f
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-07 23:09:50 +0400

    shorter and cleaner code for carousel

--- src/app/components/carousel/carousel.js
+++ src/app/components/carousel/carousel.js
@@ -7,69 +7,44 @@ import Image from 'next/image';
 
 export default function Carousel() {
   return (
-    <Swiper
-      className="w-full h-[660px]"
-      modules={[Autoplay, EffectFade]}
-      autoplay={{
-        delay: 3500,
-        disableOnInteraction: false,
-        waitForTransition: true // make sure the animation is finished before the next slide
-      }}
-      loop
-      effect="fade"
-      speed={1200}
-      allowTouchMove={false}
-    >
-      <SwiperSlide className="relative overflow-hidden">
-        <div className="zoom-wrapper absolute inset-0">
-          <Image
-            src="/images/test.png"
-            fill
-            className="object-cover"
-            alt="house of inasal foods"
-            priority // always in the first (image)
-          />
-        </div>
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/30 to-transparent z-10" />
-        <div className="absolute bottom-20 left-10 text-white z-10">
-          <h1 className="text-4xl font-bold">House of Inasal</h1>
-          <p className="mt-2">Authentic Filipino Grill</p>
-        </div>
-      </SwiperSlide>
+    <div className="relative w-full h-[660px]">
+      <Swiper
+        className="w-full h-full"
+        modules={[Autoplay, EffectFade]}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+          waitForTransition: true
+        }}
+        loop
+        effect="fade"
+        speed={1200}
+        allowTouchMove={false}
+      >
+        {['/images/test.png', '/images/test1.png', '/images/test2.png'].map((src, i) => (
+          <SwiperSlide key={i} className="relative overflow-hidden">
+            <div className="zoom-wrapper absolute inset-0">
+              <Image
+                src={src}
+                fill
+                className="object-cover"
+                alt="house of inasal foods"
+                priority={i === 0}
+              />
+            </div>
 
-      <SwiperSlide className="relative overflow-hidden">
-        <div className="zoom-wrapper absolute inset-0">
-          <Image
-            src="/images/test1.png"
-            fill
-            className="object-cover"
-            alt="house of inasal foods"
-          />
-        </div>
+            <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/30 to-transparent" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/30 to-transparent" />
-        <div className="absolute bottom-20 left-10 text-white z-10">
-          <h1 className="text-4xl font-bold">House of Inasal</h1>
-          <p className="mt-2">Authentic Filipino Grill</p>
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide className="relative overflow-hidden">
-        <div className="zoom-wrapper absolute inset-0">
-          <Image
-            src="/images/test2.png"
-            fill
-            className="object-cover"
-            alt="house of inasal foods"
-          />
-        </div>
-
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/30 to-transparent" />
-        <div className="absolute bottom-20 left-10 text-white z-10">
-          <h1 className="text-4xl font-bold">House of Inasal</h1>
-          <p className="mt-2">Authentic Filipino Grill</p>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+      {/* SINGLE OVERLAY */}
+      <div className="pointer-events-none flex flex-col items-center font-heading absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white z-20 text-center">
+        <h1 className="text-8xl font-bold [text-shadow:0px_0px_50px_rgba(0,0,0,0.57)]">
+          House of Inasal
+        </h1>
+        <p className="text-4xl mt-2">Authentic Filipino Grill</p>
+      </div>
+    </div>
   );
 }
