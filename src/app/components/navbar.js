2e38478bfb1f86c216a4aac0a3671036041127ea
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-08 23:56:37 +0400

    changed text styles

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -3,8 +3,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
-      {/* shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] save shadow */}
+    <div className="fixed top-0 left-0 w-full bg-white shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] z-50">
       <div className="max-w-[1250px] mx-auto flex items-center px-6 py-3 justify-between">
         <Link href="/" className="cursor-pointer">
           <Image
@@ -30,8 +29,8 @@ export default function Navbar() {
 
         <div className="flex flex-col cursor-pointer">
           <p className="text-sm font-semibold text-right font-ui leading-[0.78]">CALL US</p>
-          <p className="text-primary text-lg tracking-[0.2rem] font-normal font-heading">
-            0545617393
+          <p className="text-[#3A9D5D] font-primary text-xl tracking-[0.2rem] font-extrabold">
+            600560031
           </p>
         </div>
       </div>
