94beffa05f9eea9f3b1612a3b646d1388530c6e0
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-10 16:05:48 +0400

    update images

--- src/app/_components/mini-carousel.js
+++ src/app/_components/mini-carousel.js
@@ -1,4 +1,22 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import Image from 'next/image';
+
 export default function MiniCarousel() {
+  const miniCarouselImage = [
+    '/images/mini-carousel1.png',
+    '/images/mini-carousel2.png',
+    '/images/mini-carousel3.png',
+    '/images/mini-carousel4.png'
+  ];
+
+  console.log(miniCarouselImage);
+
   return (
     <>
       <div className="max-w-[1250px] w-full mt-20 mx-auto px-6">
@@ -9,6 +27,13 @@ export default function MiniCarousel() {
           <p className="text-gray-600 mt-3 max-w-xl ">
             Discover the dishes our customers love the most, freshly grilled and full of flavor.
           </p>
+        </div>
+        <div className="flex flex-row gap-12">
+          {/* <Image src="/ai.png" alt="food" width={260} height={260} /> */}
+          <Image src="/ai2.png" alt="food" width={260} height={260} />
+          <Image src="/ai3.png" alt="food" width={260} height={260} />
+          <Image src="/ai4.png" alt="food" width={260} height={260} />
+          <Image src="/ai5.png" alt="food" width={260} height={260} />
         </div>
       </div>
     </>
