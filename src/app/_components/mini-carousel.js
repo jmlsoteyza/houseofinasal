d36dc8c4f6eb093bcccae9060b8bbb3d1afd5c30
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-11 01:19:50 +0400

    fix mini-carousel styles

--- src/app/_components/mini-carousel.js
+++ src/app/_components/mini-carousel.js
@@ -9,31 +9,49 @@ import Image from 'next/image';
 
 export default function MiniCarousel() {
   const miniCarouselImage = [
-    '/images/mini-carousel1.png',
-    '/images/mini-carousel2.png',
-    '/images/mini-carousel3.png',
-    '/images/mini-carousel4.png'
+    { src: '/images/1.png', text: 'kalabasa', alt: 'pecho' },
+    { src: '/images/2.png', text: 'kalabasa', alt: 'pecho' },
+    { src: '/images/3.png', text: 'kalabasa', alt: 'pecho' },
+    { src: '/images/4.png', text: 'kalabasa', alt: 'pecho' },
+    { src: '/images/5.png', text: 'kalabasa', alt: 'pecho' }
   ];
 
   return (
-    <>
-      <div className="max-w-[1250px] w-full mt-20 mx-auto px-6">
-        <div>
-          <p className="text-sm tracking-widest font-ui uppercase text-gray-500">Our Best Seller</p>
-
-          <p className="text-[#238B45] font-heading text-3xl md:text-4xl">Inasal Favorites</p>
-          <p className="text-gray-600 mt-3 max-w-xl ">
-            Discover the dishes our customers love the most, freshly grilled and full of flavor.
-          </p>
-        </div>
-        <div className="flex flex-row gap-12">
-          <Image src="/images/1.png" alt="food" width={225} height={225} />
-          <Image src="/images/2.png" alt="food" width={225} height={225} />
-          <Image src="/images/3.png" alt="food" width={225} height={225} />
-          <Image src="/images/4.png" alt="food" width={225} height={225} />
-          <Image src="/images/5.png" alt="food" width={225} height={225} />
-        </div>
+    <div className="max-w-[1150px] w-full mt-20 mx-auto px-6">
+      {/* Section Header */}
+      <div>
+        <p className="text-sm tracking-widest font-ui uppercase text-gray-500">Our Best Seller</p>
+        <h2 className="text-[#238B45] font-heading text-3xl">Inasal Favorites</h2>
       </div>
-    </>
+
+      {/* Carousel */}
+      <div className="w-full mt-4 overflow-visible">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={80}
+          slidesPerView={4}
+          slidesPerGroup={1}
+          navigation
+          loop
+        >
+          {miniCarouselImage.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="flex flex-col items-center w-[225px]">
+                <div className="w-[225px] h-[225px] overflow-hidden">
+                  <Image
+                    className="rounded-xl object-cover"
+                    src={item.src}
+                    alt={item.alt}
+                    width={225}
+                    height={225}
+                  />
+                </div>
+                <p className="mt-2 text-center">{item.text}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
   );
 }
