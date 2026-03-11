5dc01b9f98d4798246162eb20205b4a3d2d3ecc8
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-11 21:27:25 +0400

    minor update

--- src/app/_components/promo.js
+++ src/app/_components/promo.js
@@ -9,14 +9,14 @@ export default function Promo() {
 
   return (
     <div className="mt-18 pt-1 bg-green-800 w-full">
-      <div className="py-16 max-w-[1250px] px-6 w-full mx-auto">
+      <div className="py-10 max-w-[1250px] px-6 w-full mx-auto">
         <div>
           <h1 className="text-primary font-heading text-3xl">Promo & Deals</h1>
           <p>Test</p>
         </div>
         <div className="flex flex justify-between gap-12">
           {promoImages.map((item, index) => (
-            <div key={index} className="overflow-hidden rounded-2xl w-[368px]">
+            <div key={index} className="overflow-hidden rounded-xl w-[368px]">
               <Image
                 className="object-cover"
                 src={item.src}
