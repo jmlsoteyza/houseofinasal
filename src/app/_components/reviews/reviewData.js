4c7549b044aefb938b93e828fff26a29bb86d56d
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-13 20:38:35 +0400

    add google review

---
+++ src/app/_components/reviews/reviewData.js
@@ -0,0 +1,44 @@
+export const reviews = [
+  {
+    name: 'Alexander Eya',
+    rating: 5,
+    text: 'Delicious and affordable Filipino food. Unli rice also available. Iced tea is so good. Well be back soon with more friends.',
+    date: '2 weeks ago',
+    avatar: 'SM'
+  },
+  {
+    name: 'Kahea Johayne Castillo',
+    rating: 5,
+    text: 'From the juicy chicken inasal, crispy lumpia, savory beef tapa, to the comforting beef pares. House of Inasal UAE serves all-time Pinoy favorites that hit just right. 🥰🍴 Perfect with unlimited rice and iced tea, it’s always a feast worth sharing! 😋',
+    date: '1 month ago',
+    avatar: 'KJC'
+  },
+  {
+    name: 'Ailyn Alborte',
+    rating: 5,
+    text: 'The inasal was really delicious! The flavor was just right — juicy and tasty. The staff were very accommodating and friendly. Definitely worth coming back!',
+    date: '1 month ago',
+    avatar: 'PN'
+  },
+  {
+    name: 'Anaiah Jedidiah Godoy',
+    rating: 5,
+    text: 'Excellent food quality and taste. The service was smooth and the overall experience was very satisfying. Highly recommended.',
+    date: '2 months ago',
+    avatar: 'DO'
+  },
+  {
+    name: 'Maria Calabria',
+    rating: 4,
+    text: 'House of Inasal has an innovative take on Filipino food. They have their own version of Tapsilog and HaloHalo. We’ve tasted a lot of inasal and theirs is could’ve been improved to beat Chickboy. Recommended to try their Inasal duo combo as its huge portion. Till next visit☺️',
+    date: '2 months ago',
+    avatar: 'EC'
+  },
+  {
+    name: 'Maria Calabria',
+    rating: 4,
+    text: 'House of Inasal has an innovative take on Filipino food. They have their own version of Tapsilog and HaloHalo. We’ve tasted a lot of inasal and theirs is could’ve been improved to beat Chickboy. Recommended to try their Inasal duo combo as its huge portion. Till next visit☺️',
+    date: '2 months ago',
+    avatar: 'EC'
+  }
+];
