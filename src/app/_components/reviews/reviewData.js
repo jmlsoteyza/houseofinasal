fa5a9f8b0cab2ecdf5b1b389953b52bcd4866362
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-14 04:10:33 +0400

    minor changes

--- src/app/_components/reviews/reviewData.js
+++ src/app/_components/reviews/reviewData.js
@@ -4,7 +4,7 @@ export const reviews = [
     rating: 5,
     text: 'Delicious and affordable Filipino food. Unli rice also available. Iced tea is so good. Well be back soon with more friends.',
     date: '2 weeks ago',
-    avatar: 'SM'
+    avatar: 'AE'
   },
   {
     name: 'Kahea Johayne Castillo',
@@ -18,27 +18,27 @@ export const reviews = [
     rating: 5,
     text: 'The inasal was really delicious! The flavor was just right — juicy and tasty. The staff were very accommodating and friendly. Definitely worth coming back!',
     date: '1 month ago',
-    avatar: 'PN'
+    avatar: 'AA'
   },
   {
     name: 'Anaiah Jedidiah Godoy',
     rating: 5,
     text: 'Excellent food quality and taste. The service was smooth and the overall experience was very satisfying. Highly recommended.',
     date: '2 months ago',
-    avatar: 'DO'
+    avatar: 'AJG'
   },
   {
     name: 'Maria Calabria',
     rating: 4,
     text: 'House of Inasal has an innovative take on Filipino food. They have their own version of Tapsilog and HaloHalo. We’ve tasted a lot of inasal and theirs is could’ve been improved to beat Chickboy. Recommended to try their Inasal duo combo as its huge portion. Till next visit☺️',
     date: '2 months ago',
-    avatar: 'EC'
+    avatar: '<MC>'
   },
   {
     name: 'Kim Avila',
     rating: 5,
     text: 'The food is absolutely great, and the staff are very friendly! I’ll give it 100%. We’ll surely come back again!',
     date: '2 months ago',
-    avatar: 'EC'
+    avatar: 'KA'
   }
 ];
