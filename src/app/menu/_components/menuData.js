679eb718c0c3076d499055ccad1740e630102acf
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-15 04:30:37 +0400

    menu data

---
+++ src/app/menu/_components/menuData.js
@@ -0,0 +1,19 @@
+export const tabs = [
+  'Full Menu',
+  'Inasal Specials',
+  'Sisig',
+  'Silog',
+  'Filipino Classics',
+  'Small Bites',
+  'Desserts'
+];
+
+export const menu = [
+  {
+    name: 'Pecho',
+    price: 22,
+    image: 'none yet',
+    tabs: ['Full menu, Inasal Specials'],
+    label: 'Served with unlimited rice'
+  }
+];
