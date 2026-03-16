ef7f8ab0a4d9417bfb4617fb0e64d81bd6b30a95
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-16 22:44:01 +0400

    add menu page

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,7 +1,23 @@
+'use client';
+
+import MenuCard from './_components/menuCard';
+import SideBar from './_components/sideBar';
+import { menu, tabs } from './_components/menuData';
+import { useState } from 'react';
+
 export default function Menu() {
+  const [activeTab, setActiveTab] = useState('Full Menu');
+
+  const filtered = menu.filter((item) => item.tabs.includes(activeTab));
+
   return (
-    <section className="max-w-[1250px] w-full m-h-screen mx-auto">
-      <p>Menu</p>
+    <section className="max-w-[1250px] w-full min-h-screen mx-auto flex pt-2">
+      <SideBar tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
+      <div className="flex-1 grid grid-cols-3 gap-4 p-6">
+        {filtered.map((item) => (
+          <MenuCard key={item.name} item={item} />
+        ))}
+      </div>
     </section>
   );
 }
