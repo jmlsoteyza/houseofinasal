759374a1832134724c8e3ece72c9d6a0b91c7444
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-03-25 05:23:46 +0400

    feat: add active hover and active pathname to navbar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,10 +1,21 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const links = [
+    { label: 'HOME', href: '/' },
+    { label: 'MENU', href: '/menu' },
+    { label: 'LOCATION', href: '/location' }
+  ];
+
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-[0px_0px_10px_0px_rgba(0,0,0,0.5)] z-50">
-      <div className="max-w-[78.125rem] mx-auto flex items-center px-6 py-3 justify-between">
+      <div className="max-w-[78.125rem] mx-auto flex items-center px-6 py-3 justify-between relative">
         <Link href="/" className="cursor-pointer">
           <Image
             src="/images/logo.svg"
@@ -15,16 +26,21 @@ export default function Navbar() {
           />
         </Link>
 
-        <ul className="flex gap-12 text-[#000] font-medium font-ui items-center text-sm">
-          <li>
-            <Link href="/">HOME</Link>
-          </li>
-          <li>
-            <Link href="/menu">MENU</Link>
-          </li>
-          <li>
-            <Link href="/location">LOCATION</Link>
-          </li>
+        <ul className="flex gap-12 text-[#000] font-medium font-ui text-sm">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`relative flex items-center after:absolute after:bottom-0 after:left-0 after:top-[40px] after:w-full after:h-[2.5px] after:transition-all after:duration-200 after:pointer-events-none
+        ${
+          pathname === link.href
+            ? 'text-[#2C9751] after:bg-[#2C9751]'
+            : 'text-gray-600 hover:text-[#2C9751] after:bg-transparent hover:after:bg-[#2C9751]'
+        }`}
+            >
+              {link.label}
+            </Link>
+          ))}
         </ul>
 
         <div className="flex flex-col cursor-pointer">
