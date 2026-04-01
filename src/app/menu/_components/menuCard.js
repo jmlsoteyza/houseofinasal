b871b17bfb7bfa139add61882f46a4dfef3a8aa6
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-04-01 22:31:40 +0400

    feat: add loading in menu

--- src/app/menu/_components/menuCard.js
+++ src/app/menu/_components/menuCard.js
@@ -1,19 +1,29 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function MenuCard({ item }) {
   const { name, price, label, image, alt, isObjectFit } = item;
+  const [isLoading, setIsLoading] = useState(true);
 
   const isTextLong = label.length >= 50;
 
   return (
     <div className="flex flex-col rounded-lg p-4 overflow-hidden font-ui shadow-[0_0_10px_0_rgba(0,0,0,0.1)]">
-      <div className="max-w-[225px] h-[225px] w-full self-center">
+      <div className="max-w-[225px] h-[225px] w-full self-center relative">
+        {isLoading && (
+          <div className="absolute inset-0 w-full h-full bg-gray-200 rounded-lg animate-pulse" />
+        )}
         <Image
           src={image}
           alt={alt}
           width={225}
           height={225}
-          className={`w-full h-full ${isObjectFit ? 'object-none' : 'object-cover'}`}
+          className={`w-full h-full ${isObjectFit ? 'object-none' : 'object-cover'} ${
+            isLoading ? 'opacity-0' : 'opacity-100'
+          } transition-opacity duration-300`}
+          onLoad={() => setIsLoading(false)}
         />
       </div>
 
