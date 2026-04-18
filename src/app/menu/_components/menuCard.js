a6a516401bd71cca1a8c64f063ec456c77169edd
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-04-18 09:22:39 +0400

    feat: remove tabs yellow color instead add one line on the left side when active, and changed price tag bg and text

--- src/app/menu/_components/menuCard.js
+++ src/app/menu/_components/menuCard.js
@@ -38,8 +38,8 @@ export default function MenuCard({ item, isPreloaded, onLoaded }) {
       <div className="flex flex-col flex-1">
         <div className="inline-flex flex-row align-center w-full justify-between items-center mt-4">
           <p className="font-medium text-normal text-md">{name}</p>
-          <div className="absolute top-8 right-6 bg-[#2C9751] py-2 px-3 rounded-2xl">
-            <p className="text-xs font-semibold text-white">
+          <div className="absolute top-8 right-6 bg-[#FFF186] py-2 px-3 rounded-2xl">
+            <p className="text-xs font-semibold text-[#1A1A1A]">
               {price ? (
                 `AED ${price}`
               ) : (
--- src/app/menu/_components/sideBar.js
+++ src/app/menu/_components/sideBar.js
@@ -1,15 +1,16 @@
 export default function SideBar({ tabs, activeTab, onTabChange }) {
   return (
-    <aside className=" w-full max-w-50 mt-3 hidden lg:block">
+    <aside className="w-full max-w-50 mt-3 hidden lg:block">
       {tabs.map((tab) => (
         <button
           key={tab}
           onClick={() => onTabChange(tab)}
-          // className={`w-full py-2.5 rounded-md cursor-pointer transition-colors duration-200 text-sm font-bold text-white mb-3
-          //   ${activeTab === tab ? 'bg-[#1a8a47]' : 'bg-[#27AE60] hover:bg-[#2C9751]'}`}
-          className={`w-full py-4 cursor-pointer border-b-1 border-gray-300 transition-colors duration-200 font-semibold text-sm
-            ${activeTab === tab ? 'bg-[#FBEE36] rounded-sm border-0' : ''}
-            `}
+          className={`w-full py-4 cursor-pointer border-b border-gray-300 transition-colors duration-200 text-sm text-left px-4
+            ${
+              activeTab === tab
+                ? 'border-l-[3px] border-l-[#1a5c2a] font-bold text-[#1a5c2a]'
+                : 'font-semibold text-gray-600 hover:text-[#1a5c2a]'
+            }`}
         >
           {tab}
         </button>
