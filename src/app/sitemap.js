53ae2b285fae6b7e06f0c4095d9f503387e7cd44
Jmlsoteyza <jmlsoteyza.dev@gmail.com>
2026-04-19 06:57:28 +0400

    fix: sitemap invalid date modified

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -2,31 +2,31 @@ export default function sitemap() {
   return [
     {
       url: 'https://houseofinasal.ae',
-      lastModified: new Date('2026-18-04'),
+      lastModified: new Date('2026-04-18'),
       changeFrequency: 'monthly',
       priority: 1.0
     },
     {
       url: 'https://houseofinasal.ae/menu',
-      lastModified: new Date('2026-18-04'),
+      lastModified: new Date('2026-04-18'),
       changeFrequency: 'weekly',
       priority: 0.8
     },
     {
       url: 'https://houseofinasal.ae/location',
-      lastModified: new Date('2026-18-04'),
+      lastModified: new Date('2026-04-18'),
       changeFrequency: 'yearly',
       priority: 0.7
     },
     {
       url: 'https://houseofinasal.ae/privacy-policy',
-      lastModified: new Date('2026-18-04'),
+      lastModified: new Date('2026-04-18'),
       changeFrequency: 'yearly',
       priority: 0.3
     },
     {
       url: 'https://houseofinasal.ae/terms',
-      lastModified: new Date('2026-18-04'),
+      lastModified: new Date('2026-04-18'),
       changeFrequency: 'yearly',
       priority: 0.3
     }
